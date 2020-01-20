26cf25780958e01ada0d2d021f59107bcec23a32
Lumbo1379 <36245864+Lumbo1379@users.noreply.github.com>
2020-01-20 00:07:58 +0000

    Check oxygen animation added

---
+++ Assets/OxygenMeter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenMeter : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Tab))
+            _animator.SetBool("isChecking", true);
+        else if (Input.GetKeyUp(KeyCode.Tab))
+            _animator.SetBool("isChecking", false);
+    }
+}
