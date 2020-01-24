0eea1ef70bc9ffe3b89a06f4864f9baec7979dec
Callum-Uni <cat123mrt@gmail.com>
2020-01-24 14:48:58 +0000

    Force Power
    Added force power, interacts with objects with the "Moveable" tag

---
+++ Assets/ThrustForce.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrustForce : MonoBehaviour
+{
+    [Range(0, 10)] public float Thrust;
+    [Range(0, 100)] public float Range;
+    public Camera Camera;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0)) // Left click
+            DrawRay();
+    }
+
+    private void DrawRay()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Range))
+        {
+            if (hit.transform.CompareTag("Moveable")) // CompareTag is quicker than == "Some tag"
+                ThrustObject(hit.point, hit.transform.gameObject, hit.distance);
+        }
+    }
+
+    private void ThrustObject(Vector3 hitPosition, GameObject obj, float distanceAway)
+    {
+        var fallOff = (Range - distanceAway) / Range;
+        var rb = obj.GetComponent<Rigidbody>();
+        var direction = obj.transform.position - transform.position;
+
+        rb.AddForce(direction * Thrust * fallOff);
+    }
+}
