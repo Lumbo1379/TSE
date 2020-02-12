37c9d52d65e8c16a5b9a2694aa2ed02a0e97ace1
Callum Thompson <cat123mrt@gmail.com>
2020-02-12 17:12:11 +0000

    Hologram Fade In Script

---
+++ Assets/HologramFadeIn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramFadeIn : MonoBehaviour
+{
+    public Material HologramMaterial;
+    [Range(0, 0.05f)] public float FadeInSpeed;
+
+    private float _currentFadeInValue;
+
+    private void Start()
+    {
+        _currentFadeInValue = 0.55f;
+    }
+
+    private void Update()
+    {
+        if (FadeInSpeed > 0)
+        {
+            _currentFadeInValue -= FadeInSpeed;
+            _currentFadeInValue = Mathf.Clamp(_currentFadeInValue, 0, 0.55f);
+
+
+            HologramMaterial.SetFloat("Vector1_112413B2", _currentFadeInValue);
+        }
+    }
+}
