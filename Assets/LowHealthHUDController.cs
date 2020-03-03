4226072dddb141548ecb7690a9c68c53d4a8d80b
Lumbo1379 <36245864+Lumbo1379@users.noreply.github.com>
2020-03-03 22:08:22 +0000

    HUD turns red when health low

---
+++ Assets/LowHealthHUDController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LowHealthHUDController : MonoBehaviour
+{
+    public OxygenMeter OxygenMeter;
+
+    private Image _panelHUD;
+
+    private void Start()
+    {
+        _panelHUD = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        _panelHUD.color = new Color32(Convert.ToByte(_panelHUD.color.r), Convert.ToByte(_panelHUD.color.g), Convert.ToByte(_panelHUD.color.b), Convert.ToByte(Mathf.Abs(OxygenMeter.Oxygen - 100)));
+    }
+}
--- Assets/Scripts/Player/OxygenMeter.cs
+++ Assets/Scripts/Player/OxygenMeter.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class OxygenMeter : MonoBehaviour
 {
+    public int Oxygen { get; set; }
+
     public Transform Meter;
     [Range(0, 60)] public float OxygenTick;
     public GameObject DeathPanelUI;
 
     private Animator _animator;
-    private int _oxygen;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
-        _oxygen = 100;
+        Oxygen = 100;
 
         InvokeRepeating("DepleteOxygen", OxygenTick, OxygenTick);
     }
@@ -26,7 +27,7 @@ public class OxygenMeter : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.Tab))
             _animator.SetBool("isChecking", false);
 
-        if (_oxygen < 0)
+        if (Oxygen < 0)
         {
             CancelInvoke("DepleteOxygen");
             PlayerDead();
@@ -35,16 +36,16 @@ public class OxygenMeter : MonoBehaviour
 
     private void DepleteOxygen()
     {
-        _oxygen--;
+        Oxygen--;
         Meter.localScale = new Vector3(Meter.localScale.x - 0.01f, Meter.localScale.y, Meter.localScale.z);
     }
 
     public void UpdateMeterOnSuccessfulSuck(int increase)
     {
-        _oxygen += increase;
-        _oxygen = Mathf.Clamp(_oxygen, 0, 100);
+        Oxygen += increase;
+        Oxygen = Mathf.Clamp(Oxygen, 0, 100);
 
-        Meter.localScale = new Vector3(_oxygen / 100f, Meter.localScale.y, Meter.localScale.z);
+        Meter.localScale = new Vector3(Oxygen / 100f, Meter.localScale.y, Meter.localScale.z);
     }
 
     private void PlayerDead()
