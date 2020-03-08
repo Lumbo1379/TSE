413062c0c08212a0a5d1b9a3a49ca2b695175ae4
Lumbo1379 <36245864+Lumbo1379@users.noreply.github.com>
2020-03-08 23:28:13 +0000

    Laser added
    Use "Reflectable" tag to allow it to reflect on surfaces

---
+++ Assets/Laser.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public int MaxReflections;
+    public float MaxLength;
+
+    private LineRenderer _lineRenderer;
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        var ray = new Ray(transform.position, transform.forward);
+
+        _lineRenderer.positionCount = 1;
+        _lineRenderer.SetPosition(0, transform.position);
+
+        float remainingLength = MaxLength;
+
+        for (int i = 0; i < MaxReflections; i++)
+        {
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, remainingLength))
+            {
+                _lineRenderer.positionCount += 1;
+                _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, hit.point);
+                remainingLength -= Vector3.Distance(ray.direction, hit.normal);
+                ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+
+                if (!hit.collider.CompareTag("Reflectable"))
+                    break;
+            }
+            else
+            {
+                _lineRenderer.positionCount += 1;
+                _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, ray.origin + ray.direction * remainingLength);
+            }
+        }
+    }
+}
