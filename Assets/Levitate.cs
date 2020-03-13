5b495a32377523fe17fffa6b7a06a841af450566
Lumbo1379 <36245864+Lumbo1379@users.noreply.github.com>
2020-03-13 00:08:43 +0000

    Reflectos levitate

---
+++ Assets/Levitate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levitate : MonoBehaviour
+{
+    [Range(0, 0.5f)] public float LevitateStrength;
+    [Range(0, 5)] public float Speed;
+
+    private float _startY;
+
+    private void Start()
+    {
+        _startY = transform.localPosition.y;
+    }
+
+    private void Update()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, _startY + (float)Math.Sin(Time.time * Speed) * LevitateStrength, transform.localPosition.z);
+    }
+}
