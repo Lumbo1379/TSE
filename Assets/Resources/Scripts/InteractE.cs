12de82a2c3aedd7b051f4064ccdbf6105835e1a9
Lumbo1379 <36245864+Lumbo1379@users.noreply.github.com>
2020-03-19 20:19:54 +0000

    Crosshair change
    Changes when there is an interactable in range

--- Assets/Resources/Scripts/InteractE.cs
+++ Assets/Resources/Scripts/InteractE.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractE : MonoBehaviour
 {
     [Range(0, 100)] public float InteractRange;
     public Camera Camera;
+    [ColorUsage(true, true)] public Color RegularColour;
+    [ColorUsage(true, true)] public Color InteractableInRangeColour;
+    public GameObject CrossHair;
+
+    private bool _interactableInRange;
+
+    private void Start()
+    {
+        _interactableInRange = false;
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
             ShootRay();
+
+        CheckForInteractable();
     }
 
     private void ShootRay()
@@ -22,6 +35,28 @@ public class InteractE : MonoBehaviour
         {
             if (hit.transform.CompareTag("Interactable") || hit.transform.CompareTag("Reflectable"))
                 hit.transform.gameObject.GetComponent<IInteractable>().Interact();
+        }
+    }
+
+    private void CheckForInteractable()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if ((hit.transform.CompareTag("Interactable") || hit.transform.CompareTag("Reflectable")) && !_interactableInRange)
+            {
+                _interactableInRange = true;
+                CrossHair.transform.localScale *= 3;
+                CrossHair.GetComponent<Image>().color = InteractableInRangeColour;
+            }
+            else if (!hit.transform.CompareTag("Interactable") && !hit.transform.CompareTag("Reflectable") && _interactableInRange)
+            {
+                _interactableInRange = false;
+                CrossHair.transform.localScale /= 3;
+                CrossHair.GetComponent<Image>().color = RegularColour;
+            }
         }
     }
 }
